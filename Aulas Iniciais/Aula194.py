b99b9cb8a6a376049186c017f54df8eea94a8d02
Gabriel Garçoa <gabre01102001@gmail.com>
2024-05-06 21:14:53 -0300

    Exemplos de jupyter notebook

--- Aulas Iniciais/Aula194.py
+++ Aulas Iniciais/Aula194.py
@@ -22,3 +22,15 @@
 # Linux/Mac: ping 127.0.0.1 -c 4
 
 import subprocess
+import sys
+
+
+print(sys.platform)
+
+cmd = ['ping', '127.0.0.1']
+
+proc = subprocess.run(
+    cmd, capture_output=True, text=True, encoding='cp850'
+)
+
+print(proc.stdout)
